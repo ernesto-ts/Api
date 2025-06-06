880ee16affe80b8f17f40e9ae9834f0fab89ab14
Ernesto Taborda Schneider <ernesto@rede.ulbra.br>
2025-06-06 19:25:52 -0300

    2

--- api/index.js
+++ api/index.js
@@ -1,4 +1,8 @@
+// import app from '../server.js';
+// import serverless from 'serverless-http';
+
+// export const handler = serverless(app);
 import app from '../server.js';
 import serverless from 'serverless-http';
 
-export const handler = serverless(app);
+export default serverless(app);
