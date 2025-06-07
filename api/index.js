924bb5cef5e003bcaf2f1dd09080e8f7770bb2ba
Ernesto Taborda Schneider <ernesto@rede.ulbra.br>
2025-06-06 21:34:29 -0300

    4

--- api/index.js
+++ api/index.js
@@ -5,5 +5,7 @@
 import app from '../server.js';
 import serverless from 'serverless-http';
 const port = process.env.PORT || 3000;
-
+app.get('/', (req, res) => {
+  res.send('Hello from the serverless function!');
+});
 export default serverless(app);
